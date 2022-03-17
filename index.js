913639a519e3f0a85a2425428075f7fcba5c0500
yugmani <yugmani@yahoo.com>
2022-03-17 12:27:18 -0700

    musketeers done

--- index.js
+++ index.js
@@ -2,5 +2,41 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// MUSKETEERS
+// 1.
+const musketeers = ['Athos', 'Porthos', 'Aramis'];
+
+// 2.
+for (let i = 0; i < musketeers.length; i++) {
+  console.log(musketeers[i]);
+}
+
+///Athos, Porthos, Aramis
+
+//3.
+musketeers.push("D'Artagnan");
+
+console.log(musketeers[musketeers.length - 1]); //D'Artagnan
+
+//4.
+const forEachArray = musketeers.forEach((musket) => console.log(musket));
+
+console.log(forEachArray); //Athos Porthos Aramis D'Artagnan
+
+//5.
+let poorAramis = musketeers.splice(2, 1);
+
+console.log(poorAramis); // ["Aramis"]
+
+//6.
+for (const musket of musketeers) {
+  console.log(musket);
+}
+
+// Athos Porthos D'Artagnan
+
+// SUM OF VALUES
+const values = [3, 11, 7, 2, 9, 10];
